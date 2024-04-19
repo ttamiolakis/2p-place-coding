276be43e90f353d16dd98058c8784cc539c2cf73
ttamiolakis <100780366+ttamiolakis@users.noreply.github.com>
2024-04-19 12:00:50 +0200

    jason ftw

--- spatial_coding_functions.py
+++ spatial_coding_functions.py
@@ -34,12 +34,23 @@ def make_firing_rate_maps(data,num_rounds,num_units,num_bins):
 #taking the zscore flurorescence. finding the peaks in it and making a binary panda frame out of it.
 #meaning with 0 and 1. zero is events and 1 is events
 
+# def make_binary(data,peak_threshold=3,peak_distance=10):
+#         #the data file will be the the firing rate map of every cell
+#         data_binarized=np.zeros_like(data) #making a copy of the original file
+#         for unit in range(data.shape[0]):
+#             peaks_all_data=peak_local_max(data[unit],threshold_abs=peak_threshold,min_distance=peak_distance,exclude_border=False)
+#             data_binarized[unit][peaks_all_data[:, 0], peaks_all_data[:, 1]] = 1
+#         return data_binarized
+
 def make_binary(data,peak_threshold=3,peak_distance=10):
         #the data file will be the the firing rate map of every cell
         data_binarized=np.zeros_like(data) #making a copy of the original file
+        #iterating through cells
         for unit in range(data.shape[0]):
-            peaks_all_data=peak_local_max(data,threshold_abs=peak_threshold,min_distance=peak_distance,exclude_border=False)
-            data_binarized[peaks_all_data[:, 0], peaks_all_data[:, 1]] = 1
+            for round in range(data.shape[1]):
+                 data_per_round=data[unit][round]
+                 peaks, _=find_peaks(data_per_round, height=peak_threshold,distance=peak_distance)
+                 data_binarized[unit][round][peaks]=1
         return data_binarized
-        
+    
 #def calcium_trace(data,cell_number):
